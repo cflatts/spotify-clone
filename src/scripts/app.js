d36789ad7d59cceab96abd60487077ca942e1cb5
Caitlin Flattery <caitlinflattery@gmail.com>
2016-11-02 17:44:19 -0500

    extended backbone router and created new instance

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Backbone from 'backbone'
 
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
-		}
-	})
+    var AppRouter = Backbone.Router.extend({
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+    })
+    var appRouter = new AppRouter()
 }
 
 app()
